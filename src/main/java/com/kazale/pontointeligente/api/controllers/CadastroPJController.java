88892ec436abe09cdd3262e296cce66449bf4dea
paulo.carvalho <paulo.carvalho@trt8.jus.br>
2018-10-19 14:27:25 -0300

    Aula 16 concluida.

---
+++ src/main/java/com/kazale/pontointeligente/api/controllers/CadastroPJController.java
@@ -0,0 +1,90 @@
+package com.kazale.pontointeligente.api.controllers;
+
+import java.security.NoSuchAlgorithmException;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kazale.pontointeligente.api.dtos.CadastroPJDto;
+import com.kazale.pontointeligente.api.entities.Empresa;
+import com.kazale.pontointeligente.api.entities.Funcionario;
+import com.kazale.pontointeligente.api.response.Response;
+import com.kazale.pontointeligente.api.services.EmpresaService;
+import com.kazale.pontointeligente.api.services.FuncionarioService;
+
+@RestController
+@RequestMapping("/api/cadastrar-pj")
+@CrossOrigin(origins = "*")
+public class CadastroPJController {
+	
+		private static final Logger log = LoggerFactory.getLogger(CadastroPJController.class);
+		
+		@Autowired
+		private FuncionarioService funcionarioService;
+		
+		@Autowired
+		private EmpresaService empresaService;
+
+		public CadastroPJController() {
+			super();
+		}
+		
+		
+		@PostMapping
+		public ResponseEntity<Response<CadastroPJDto>> cadastrar(@Valid @RequestBody CadastroPJDto cadastroPJDto, 
+				BindingResult result) throws NoSuchAlgorithmException {
+			log.info("Cadastrando PJ: {}", cadastroPJDto.toString());
+			
+			Response<CadastroPJDto> response = new Response<CadastroPJDto>();
+			
+			validarDadosExistentes(cadastroPJDto, result);
+			
+			Empresa empresa = this.converDtoParaFuncionario(cadastroPJDto, result);
+			Funcionario funcionario = this.converterDtoParaFuncionario(cadastroPJDto, result);
+			
+			if(result.hasErrors()) {
+				log.error("Erro validando dados de cadastro PJ: {}", result.getAllErrors());
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()) );
+				return ResponseEntity.badRequest().body(response);
+			}
+			
+			this.empresaService.persistir(empresa);
+			funcionario.setEmpresa(empresa);
+			this.funcionarioService.persistir(funcionario);
+			
+			response.setData(this.converterCadastroPJDto(funcionario));
+			
+			
+			return ResponseEntity.ok(response);
+		}
+
+		
+		/**
+		 * Verifica se a empresa ou funcionário já existem na base de dados.
+		 * @param cadastroPJDto
+		 * @param result
+		 */
+		private void validarDadosExistentes(CadastroPJDto cadastroPJDto, BindingResult result) {
+			this.empresaService.buscaPorCnpj(cadastroPJDto.getCnpj())
+				.ifPresent(emp ->  result.addError(new ObjectError("empresa", "Empresa já existe.")));
+			
+			this.funcionarioService.buscarPorCpf(cadastroPJDto.getCpf())
+				.ifPresent(func -> result.addError(new ObjectError("funcionario", "CPF já existente.")));
+			
+			this.funcionarioService.buscaPorEmail(cadastroPJDto.getEmail())
+				.ifPresent(func -> result.addError(new ObjectError("funcionario", "Email já existente.")));
+		}
+		
+		
+}
